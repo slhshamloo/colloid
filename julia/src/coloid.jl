3356725be5f8835f3aca2564c776518c73f471f1
Saleh Shamloo <SLHHero@gmail.com>
2022-07-18 15:26:24 +0430

    added circular moves and parameter structure

--- julia/src/coloid.jl
+++ julia/src/coloid.jl
@@ -21,6 +21,15 @@ struct Coloid{P<:AbstractPolygon, F<:AbstractFloat}
     end
 end
 
+struct ColoidSimParams
+    all_moves::Integer
+    all_rotations::Integer
+    accepted_moves::Integer
+    accepted_rotations::Integer
+    energy::Vector{<:AbstractFloat}
+    nematic_order::Vector{<:AbstractFloat}
+end
+
 function Coloid{F}(particle_count::Integer, particle_sidenum::Integer,
         particle_radius::Real, boxsize::Tuple{<:Real, <:Real}) where {F<:AbstractFloat}
     if particle_sidenum % 2 == 0
@@ -53,9 +62,29 @@ function crystal_initialize!(coloid::Coloid, gridwidth::Integer,
     end
 end
 
-square_nematic_order(coloid::Coloid) = mean(square_nematic_order, coloid.particles)
+function energy(coloid::Coloid, interaction_strength::Real, index::Integer)
+    esum = 0.0
+    for j in union(1:index-1, index+1:length(coloid.particles))
+        esum += interaction_strength * potential(coloid.particles[index],
+            coloid.particles[j], periodic_boundary_shift(coloid.boxsize))
+    end
+    return esum
+end
 
-function square_nematic_order(poly::AbstractPolygon)
+function energy(coloid::Coloid, interaction_strength::Real)
+    esum = 0.0
+    for i in 1:length(coloid.particles)-1
+        for j in i+1:length(coloid.particles)
+            esum += interaction_strength * potential(coloid.particles[i],
+                coloid.particles[j], periodic_boundary_shift(coloid.boxsize))
+        end
+    end
+    return esum
+end
+
+nematic_order(coloid::Coloid) = mean(nematic_order, coloid.particles)
+
+function nematic_order(poly::AbstractPolygon)
     return (3 * max(poly.normals[1]^2, poly.normals[2]^2) - 1) / 2
 end
 
@@ -97,8 +126,9 @@ function simulate!(coloid::Coloid, move_radius::Real, rotation_span::Real;
 
     accepted_rotations, accepted_moves = 0, 0
     if calculate
-        nematic_orders = Vector{F}(undef, steps + 1)
-        nematic_orders[1] = square_nematic_order(coloid)
+        energies, nematic_orders = Vector{F}(undef, steps + 1), Vector{F}(undef, steps + 1)
+        energies[1] = energies(coloid, interaction_strength)
+        nematic_orders[1] = nematic_order(coloid)
     end
 
     # precalculate random numbers to speed-up calculations
@@ -109,26 +139,28 @@ function simulate!(coloid::Coloid, move_radius::Real, rotation_span::Real;
     for step in 1:steps
         particle = coloid.particles[random_choices[step]]
         if calculate
-            nematic_orders[step+1] = nematic_orders[step] - square_nematic_order(particle)
+            nematic_orders[step+1] = nematic_orders[step] - nematic_order(particle)
         end
         if move_or_rotate[step]
-            accepted_moves += _one_mc_movement!(coloid, particle,
-                (rnd[1, step], rnd[2, step], rnd[3, step]),
-                move_radius, interaction_strength)
+            acc, ΔE = _one_mc_movement!(coloid, particle,
+                (rnd[1, step], rnd[2, step], rnd[3, step]), move_radius,
+                interaction_strength)
+            accepted_moves += acc
         else
-            accepted_rotations += _one_mc_rotation!(coloid, particle,
-                (rnd[1, step], rnd[2, step], rnd[3, step]),
-                rotation_span, interaction_strength)
+            acc, ΔE += _one_mc_rotation!(coloid, particle,
+                (rnd[1, step], rnd[2, step]), rotation_span, interaction_strength)
+            accepted_rotations += acc
         end
         if calculate
-            nematic_orders[step+1] += square_nematic_order(particle)
+            nematic_orders[step+1] += nematic_order(particle)
+            energies[step+1] = energies[step] + ΔE
         end
     end
 
     all_moves = count(move_or_rotate)
     if calculate
-        return (nematic_orders, accepted_moves / all_moves,
-            accepted_rotations / (steps - all_moves))
+        return ColoidSimParams(all_moves, steps - all_moves, accepted_moves,
+            accepted_rotations, energies, nematic_order)
     else
         return accepted_moves / all_moves, accepted_rotations / (steps - all_moves)
     end
@@ -144,7 +176,10 @@ function _one_mc_movement!(coloid::Coloid, particle::AbstractPolygon,
     coloid._temp_vertices .= particle.vertices
     coloid._temp_center .= particle.center
 
-    move!(particle, (move_radius * (2rnd[1] - 1), move_radius * (2rnd[2] - 1)))
+    r = move_radius * rnd[1]
+    θ = 2π * rnd[2]
+
+    move!(particle, (r * cos(θ), r * sin(θ)))
     apply_periodic_boundary!(particle, coloid.boxsize)
 
     if isinf(interaction_strength)
@@ -152,19 +187,19 @@ function _one_mc_movement!(coloid::Coloid, particle::AbstractPolygon,
                 filter(!=(particle), coloid.particles))
             particle.vertices .= coloid._temp_vertices
             particle.center .= coloid._temp_center
-            return 0
+            return 0, 0
         else
-            return 1
+            return 1, 0
         end
     else
         newpot = sum(p -> potential(p, particle, periodic_boundary_shift(coloid.boxsize)),
             filter(!=(particle), coloid.particles))
         if ℯ^(interaction_strength * (prevpot - newpot)) > rnd[3]
-            return 1
+            return 1, newpot - prevpot
         else
             particle.vertices .= coloid._temp_vertices
             particle.center .= coloid._temp_center
-            return 0
+            return 0, 0
         end
     end
 end
@@ -186,19 +221,19 @@ function _one_mc_rotation!(coloid::Coloid, particle::AbstractPolygon,
                 filter(!=(particle), coloid.particles))
             particle.vertices .= coloid._temp_vertices
             particle.normals .= coloid._temp_normals
-            return 0
+            return 0, inf
         else
-            return 1
+            return 1, inf
         end
     else
         newpot = sum(p -> potential(p, particle, periodic_boundary_shift(coloid.boxsize)),
             filter(!=(particle), coloid.particles))
-        if ℯ^(interaction_strength * (prevpot - newpot)) > rnd[3]
-            return 1
+        if ℯ^(interaction_strength * (prevpot - newpot)) > rnd[2]
+            return 1, newpot - prevpot
         else
             particle.vertices .= coloid._temp_vertices
             particle.normals .= coloid._temp_normals
-            return 0
+            return 0, newpot - prevpot
         end
     end
 end
