f54c1de1deea78478b3b7cf568ea2c3db0d13923
Saleh Shamloo <SLHHero@gmail.com>
2022-11-05 20:47:52 +0330

    implemented simple trajectory recorder

--- julia/src/record.jl
+++ julia/src/record.jl
@@ -1,1 +1,32 @@
 abstract type AbstractRecorder end
+
+struct TrajectoryRecorder
+    snapshots::Vector{ColloidSnapshot}
+    cond::Function
+
+    function TrajectoryRecorder(cond)
+        new(ColloidSnapshot[], cond)
+    end
+end
+
+struct ColloidSnapshot
+    centers::AbstractMatrix
+    angles::AbstractVector
+    boxsize::Tuple{<:Real, <:Real}
+end
+
+function record(recorder::TrajectoryRecorder, colloid::Colloid, timestep::Integer)
+    if recorder.cond(timestep)
+        push!(recorder.snapshots, get_snapshot(colloid))
+    end
+end
+
+function get_snapshot(colloid::Colloid)
+    normals = Array(colloid.normals)
+    angles = Vector{eltype(centers)}(undef, particle_count(colloid))
+    for particle in eachindex(angles)
+        cosine = normals[1, 1, particle]
+        angles[particle] = abs(cosine) > 1 ? acos(round(cosine)) : acos(cosine)
+    end
+    return ColloidSnapshot(Matrix(colloid.centers), angles, Tuple(colloid.boxsize))
+end
