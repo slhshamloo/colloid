5d07090a1baf2f2f68fad5fe41eba7c89adc6309
Saleh Shamloo <SLHHero@gmail.com>
2023-01-08 13:55:29 +0330

    fixed cell list variable in functions

--- julia/src/cellseq.jl
+++ julia/src/cellseq.jl
@@ -4,7 +4,7 @@ struct SeqCellList <: CellList
     cells::Matrix{Vector{Int}}
     width::Tuple{<:Real, <:Real}
 
-    function CellList(colloid::Colloid)
+    function SeqCellList(colloid::Colloid)
         d = 2 * colloid.radius
         width = (d + (colloid.boxsize[1] % d) / (colloid.boxsize[1] ÷ d),
                  d + (colloid.boxsize[2] % d) / (colloid.boxsize[2] ÷ d))
@@ -42,34 +42,32 @@ function has_overlap(colloid::Colloid, cell_list::SeqCellList,
             return true
         end
     end
-    if has_orthogonal_overlap(colloid, cell_list.cells, i, j, idx)
+    if has_orthogonal_overlap(colloid, cell_list, i, j, idx)
         return true
     end
-    if has_diagonal_overlap(colloid, cell_list.cells, i, j, idx)
+    if has_diagonal_overlap(colloid, cell_list, i, j, idx)
         return true
     end
     return false
 end
 
 function has_overlap(colloid::Colloid, cell_list::SeqCellList)
-    for idx in CartesianIndices(cell_list.cells)
-        i, j = Tuple(idx)
-        for m in eachindex(cell_list.cells[idx])
-            for n in m+1:length(cell_list.cells[idx])
-                if is_overlapping(colloid, cell_list.cells[idx][m],
-                                  cell_list.cells[idx][n])
+    for cell in CartesianIndices(cell_list.cells)
+        i, j = Tuple(cell)
+        for m in eachindex(cell_list.cells[cell])
+            idx = cell_list.cells[cell][m]
+            for n in m+1:length(cell_list.cells[cell])
+                if is_overlapping(colloid, idx, cell_list.cells[cell][n])
                     return true
                 end
             end
             if (i + j) % 2 == 0
-                if has_orthogonal_overlap(colloid, cell_list.cells, i, j,
-                                          cell_list.cells[idx][m])
+                if has_orthogonal_overlap(colloid, cell_list, i, j, idx)
                     return true
                 end
             end
             if i % 2 == 0
-                if has_diagonal_overlap(colloid, cell_list.cells, i, j,
-                                        cell_list.cells[idx][m])
+                if has_diagonal_overlap(colloid, cell_list, i, j, idx)
                     return true
                 end
             end
@@ -80,22 +78,20 @@ end
 
 function count_overlaps(colloid::Colloid, cell_list::SeqCellList)
     overlap_count = 0
-    for idx in CartesianIndices(cell_list.cells)
-        i, j = Tuple(idx)
-        for m in eachindex(cell_list.cells[idx])
-            for n in m+1:length(cell_list.cells[idx])
-                if is_overlapping(colloid, cell_list.cells[idx][m],
-                                  cell_list.cells[idx][n])
+    for cell in CartesianIndices(cell_list.cells)
+        i, j = Tuple(cell)
+        for m in eachindex(cell_list.cells[cell])
+            idx = cell_list.cells[cell][m]
+            for n in m+1:length(cell_list.cells[cell])
+                if is_overlapping(colloid, idx, cell_list.cells[cell][n])
                     overlap_count += 1
                 end
             end
             if (i + j) % 2 == 0
-                overlap_count += count_orthogonal_overlaps(
-                    colloid, cell_list.cells, i, j, cell_list.cells[idx][m])
+                overlap_count += count_orthogonal_overlaps(colloid, cell_list, i, j, idx)
             end
             if i % 2 == 0
-                overlap_count += count_diagonal_overlaps(
-                    colloid, cell_list.cells, i, j, cell_list.cells[idx][m])
+                overlap_count += count_diagonal_overlaps(colloid, cell_list, i, j, idx)
             end
         end
     end
@@ -145,7 +141,7 @@ end
     for n in cell_list.cells[(i == 1 ? lx : i - 1), j]
         if is_overlapping(colloid, m, n) count += 1 end
     end
-    for n in cells[i, (j == 1 ? ly : j - 1)]
+    for n in cell_list.cells[i, (j == 1 ? ly : j - 1)]
         if is_overlapping(colloid, m, n) count += 1 end
     end
     for n in cell_list.cells[(i == lx ? 1 : i + 1), j]
