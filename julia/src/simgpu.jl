77cb758f679ae26c84e1d6544dfd4a5c6e2c01a6
slhshamloo <SLHHero@gmail.com>
2023-09-10 18:14:15 +0330

    reverted constraint handling for update support

--- julia/src/simgpu.jl
+++ julia/src/simgpu.jl
@@ -1,4 +1,4 @@
-function apply_step!(sim::Simulation, cell_list::CuCellList, constraints::RawConstraints)
+function apply_step!(sim::Simulation, cell_list::CuCellList)
     blockthreads = numthreads[1] * numthreads[2]
     sweeps = ceil(Int, mean(cell_list.counts))
 
@@ -7,6 +7,7 @@ function apply_step!(sim::Simulation, cell_list::CuCellList, constraints::RawCon
     randchoices = CUDA.rand(Bool, size(cell_list.cells, 2),
                             size(cell_list.cells, 3), sweeps)
     accept = CuArray(zeros(Int, 4))
+    constraints = build_raw_constraints(sim.constraints, sim.numtype)
 
     for sweep in 1:sweeps
         maxcount = maximum(cell_list.counts)
