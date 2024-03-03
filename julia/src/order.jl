4690d90a6cf0cdc67262c294cb869908d43aae12
slhshamloo <SLHHero@gmail.com>
2024-03-03 21:27:55 +0330

    generalized order parameter calculation further

--- julia/src/order.jl
+++ julia/src/order.jl
@@ -1,27 +1,29 @@
-function katic_order(particles::RegularPolygons, k::Integer; numtype::DataType = Float32)
+function katic_order(particles::RegularPolygons, k::Integer,
+        neighbors::Integer = k; numtype::DataType = Float32)
     if isa(particles.centers, CuArray)
-        return korder(particles, CuCellList(particles), k,
-            (kk, neighbor_r, neighbor_angle) -> exp(1im * kk * neighbor_angle) / kk,
+        return local_order(particles, CuCellList(particles), k,
+            (neighbor_r, neighbor_angle) -> exp(1im * k * neighbor_angle) / neighbors,
             numtype=numtype)
     else
-        return korder(particles, SeqCellList(particles), k,
-            (kk, neighbor_r, neighbor_angle) -> sum(exp.(1im * kk * neighbor_angle)) / kk,
+        return local_order(particles, SeqCellList(particles), k,
+            (neighbor_r, neighbor_angle) -> sum(exp.(1im * k * neighbor_angle)) / neighbors,
             numtype=numtype)
     end
 end
 
-@inline katic_order(particles::RegularPolygons, cell_list::SeqCellList, k::Integer;
-    numtype::DataType = Float32) = korder(particles, cell_list, k,
-        (kk, neighbor_r, neighbor_angle) -> sum(exp.(1im * kk * neighbor_angle)) / kk,
-        numtype=numtype)
+@inline katic_order(particles::RegularPolygons, cell_list::SeqCellList, k::Integer,
+    neighbors::Integer = k; numtype::DataType = Float32) = local_order(
+        particles, cell_list, k, (neighbor_r, neighbor_angle) -> sum(exp.(
+            1im * k * neighbor_angle)) / neighbors, numtype=numtype)
 
-@inline katic_order(particles::RegularPolygons, cell_list::CuCellList, k::Integer;
-    numtype::DataType = Float32) = korder(particles, cell_list, k,
-        (kk, neighbor_r, neighbor_angle) -> exp(1im * kk * neighbor_angle) / kk,
-        numtype=numtype)
+@inline katic_order(particles::RegularPolygons, cell_list::CuCellList, k::Integer,
+    neighbors::Integer = k; numtype::DataType = Float32) = local_order(
+        particles, cell_list, neighbors, (neighbor_r, neighbor_angle) -> exp(
+            1im * k * neighbor_angle) / neighbors, numtype=numtype)
 
-function korder(particles::RegularPolygons, cell_list::SeqCellList, k::Integer,
-                orderfunc::Function; iscomplex::Bool = true, numtype::DataType = Float32)
+function local_order(
+        particles::RegularPolygons, cell_list::SeqCellList, neighbors::Integer,
+        orderfunc::Function; iscomplex::Bool = true, numtype::DataType = Float32)
     orders = (iscomplex ? zeros(Complex{numtype}, particlecount(particles))
                         : zeros(numtype, particlecount(particles)))
     for cell in CartesianIndices(cell_list.cells)
@@ -36,19 +38,26 @@ function korder(particles::RegularPolygons, cell_list::SeqCellList, k::Integer,
                     push!(neighbor_angle, angle)
                 end
             end
-            if length(neighbor_r) >= k
-                local_indices = partialsortperm(neighbor_r, 1:k)
-                neighbor_r = neighbor_r[local_indices]
-                neighbor_angle = neighbor_angle[local_indices]
-                orders[idx] += orderfunc(k, neighbor_r, neighbor_angle)
+            if neighbors > 0
+                if length(neighbor_r) >= neighbors
+                    local_indices = partialsortperm(neighbor_r, 1:neighbors)
+                    neighbor_r = neighbor_r[local_indices]
+                    neighbor_angle = neighbor_angle[local_indices]
+                    orders[idx] += orderfunc(neighbors, neighbor_r, neighbor_angle)
+                end
+            else
+                if length(neighbor_r) > 0
+                    orders[idx] += orderfunc(neighbor_r, neighbor_angle)
+                end
             end
         end
     end
     return orders
 end
 
-function korder(particles::RegularPolygons, cell_list::CuCellList, k::Integer,
-                orderfunc::Function; iscomplex::Bool = true, numtype::DataType = Float32)
+function local_order(
+        particles::RegularPolygons, cell_list::CuCellList, neighbors::Integer,
+        orderfunc::Function; iscomplex::Bool = true, numtype::DataType = Float32)
     maxcount = maximum(cell_list.counts)
     groupcount = 9 * maxcount
     groups_per_block = numthreads ÷ groupcount
@@ -56,22 +65,25 @@ function korder(particles::RegularPolygons, cell_list::CuCellList, k::Integer,
     orders = (iscomplex ? zeros(Complex{numtype}, particlecount(particles))
                         : zeros(numtype, particlecount(particles)))
     orders = CuArray(orders)
-    @cuda(threads=numthreads, blocks=numblocks,
-          shmem = 2 * groups_per_block * (groupcount + k) * sizeof(numtype),
-          korder_parallel!(particles, cell_list, orders, orderfunc, k,
-                           maxcount, groupcount, groups_per_block, numtype))
+    if neighbors > 0
+        @cuda(threads=numthreads, blocks=numblocks,
+              shmem = 2 * groups_per_block * (groupcount + neighbors) * sizeof(numtype),
+              local_order_parallel!(particles, cell_list, orders, orderfunc, neighbors,
+                                    maxcount, groupcount, groups_per_block, numtype))
+    else
+        @cuda(threads=numthreads, blocks=numblocks,
+              local_order_parallel!(particles, cell_list, orders, orderfunc,
+                                    maxcount, groupcount, groups_per_block))
+    end
     return Vector(orders)
 end
 
-function korder_parallel!(particles::RegularPolygons, cell_list::CuCellList,
-        orders::CuDeviceVector, orderfunc::Function, k::Integer, maxcount::Integer,
+function local_order_parallel!(particles::RegularPolygons, cell_list::CuCellList,
+        orders::CuDeviceVector, orderfunc::Function, neighbors::Integer, maxcount::Integer,
         groupcount::Integer, groups_per_block::Integer, numtype::DataType)
     active_threads = groups_per_block * groupcount
-    shared_memory = CuDynamicSharedArray(numtype, 2 * groups_per_block * (groupcount + k))
-    group_r = @view shared_memory[1:active_threads]
-    group_angle = @view shared_memory[active_threads+1:2*active_threads]
-    neighbor_r = @view shared_memory[2*active_threads+1:2*active_threads+groups_per_block*k]
-    neighbor_angle = @view shared_memory[2*active_threads+groups_per_block*k+1:end]
+    group_r, group_angle, neighbor_r, neighbor_angle = get_local_order_shmem(
+        active_threads, groups_per_block, groupcount, neighbors, numtype)
 
     is_thread_active = threadIdx().x <= active_threads
     group, thread = divrem(threadIdx().x - 1, groupcount)
@@ -81,7 +93,7 @@ function korder_parallel!(particles::RegularPolygons, cell_list::CuCellList,
         is_thread_active = particle <= particlecount(particles)
         if is_thread_active
             i, j = get_cell_list_indices(particles, cell_list, particle)
-            is_thread_active = count_neighbors(cell_list.counts, i, j) >= k
+            is_thread_active = count_neighbors(cell_list.counts, i, j) >= neighbors
             if is_thread_active
                 calc_neighbor!(particles, cell_list, group_r, group_angle,
                                particle, i, j, maxcount, thread)
@@ -90,14 +102,53 @@ function korder_parallel!(particles::RegularPolygons, cell_list::CuCellList,
     end
     CUDA.sync_threads()
 
-    k_partition_select!(group_r, group_angle, neighbor_r, neighbor_angle, k,
+    k_partition_select!(group_r, group_angle, neighbor_r, neighbor_angle, neighbors,
                         group, thread, groupcount, is_thread_active)
     if is_thread_active && thread == 0
-        for iteridx in (group - 1) * k + 1 : group * k
-            orders[particle] += orderfunc(k, neighbor_r[iteridx], neighbor_angle[iteridx])
+        for iteridx in (group - 1) * neighbors + 1 : group * neighbors
+            orders[particle] += orderfunc(neighbor_r[iteridx], neighbor_angle[iteridx])
         end
     end
     return
+end
+
+function local_order_parallel!(particles::RegularPolygons, cell_list::CuCellList,
+        orders::CuDeviceVector, orderfunc::Function, maxcount::Integer,
+        groupcount::Integer, groups_per_block::Integer)
+    if threadIdx().x <= groups_per_block * groupcount
+        group, thread = divrem(threadIdx().x - 1, groupcount)
+        group += 1
+        particle = (blockIdx().x - 1) * groups_per_block + group
+        if particle <= particlecount(particles)
+            i, j = get_cell_list_indices(particles, cell_list, particle)
+            if count_neighbors(cell_list.counts, i, j) > 0
+                ineighbor, jneighbor, kneighbor = get_neighbor_indices(
+                    cell_list, thread, maxcount, i, j)
+                if kneighbor <= cell_list.counts[ineighbor, jneighbor]
+                    neighbor = cell_list.cells[kneighbor, ineighbor, jneighbor]
+                    if particle != neighbor
+                        r, theta = get_dist_and_angle(particles, particle, neighbor)
+                        CUDA.@atomic orders[particle] += orderfunc(r, theta)
+                    end
+                end
+            end
+        end
+    end
+    return
+end
+
+@inline function get_local_order_shmem(active_threads::Integer,
+        groups_per_block::Integer, groupcount::Integer,
+        neighbors::Integer, numtype::DataType)
+    shared_memory = CuDynamicSharedArray(
+        numtype, 2 * groups_per_block * (groupcount + neighbors))
+    group_r = @view shared_memory[1:active_threads]
+    group_angle = @view shared_memory[active_threads + 1 : 2 * active_threads]
+    neighbor_r = @view shared_memory[
+        2 * active_threads + 1 : 2 * active_threads + groups_per_block * neighbors]
+    neighbor_angle = @view shared_memory[
+        2 * active_threads + groups_per_block * neighbors + 1 : end]
+    return group_r, group_angle, neighbor_r, neighbor_angle
 end
 
 function calc_neighbor!(particles::RegularPolygons, cell_list::CuCellList,
